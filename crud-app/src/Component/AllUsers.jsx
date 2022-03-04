f564c9811826d08b109f2b770717a3ddd3d0b170
mohussain237 <mohussain237@gmail.com>
2022-03-04 17:25:40 +0530

    adding UI for CRUD app

---
+++ crud-app/src/Component/AllUsers.jsx
@@ -0,0 +1,77 @@
+import react, { useState, useEffect } from 'react';
+import { Table, TableHead, TableCell, Paper, TableRow, TableBody, Button, makeStyles } from '@material-ui/core'
+import { getUsers, deleteUser } from '../Service/api';
+import { Link } from 'react-router-dom';
+
+
+const useStyles = makeStyles({
+    table: {
+        width: '60%',
+        marginLeft: '20%',
+        marginTop:"5%"
+    },
+    thead: {
+        '& > *': {
+            fontSize: 20,
+            background: "rgb(62, 38, 59)",
+            color: '#FFFFFF'
+        }
+    },
+    row: {
+        '& > *': {
+            fontSize: 18
+        }
+    }
+})
+
+
+const AllUsers = () => {
+    const [users, setUsers] = useState([]);
+    const classes = useStyles();
+
+    useEffect(() => {
+        getAllUsers();
+    }, []);
+
+    const deleteUserData = async (id) => {
+        await deleteUser(id);
+        getAllUsers();
+    }
+
+    const getAllUsers = async () => {
+        let response = await getUsers();
+        setUsers(response.data);
+    }
+
+    return (
+        <Table className={classes.table}>
+            <TableHead>
+                <TableRow className={classes.thead}>
+                    
+                    <TableCell>Name</TableCell>
+                    <TableCell>Username</TableCell>
+                    <TableCell>Email</TableCell>
+                    <TableCell>Phone</TableCell>
+                   
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {users.map((user) => (
+                    <TableRow className={classes.row} key={user.id}>
+                        
+                        <TableCell>{user.name}</TableCell>
+                        <TableCell>{user.username}</TableCell>
+                        <TableCell>{user.email}</TableCell>
+                        <TableCell>{user.phone}</TableCell>
+                        <TableCell>
+                            <Button color="primary" variant="contained" style={{marginRight:10}} component={Link} to={`/edit/${user._id}`}>Edit</Button>
+                            <Button color="secondary" variant="contained" onClick={() => deleteUserData(user._id)}>Delete</Button>
+                        </TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
+    )
+}
+
+export default AllUsers;
---
+++ crud-app/src/Component/NotFound.jsx
@@ -0,0 +1,12 @@
+const NotFound = () => {
+  return (
+    <img
+      src={
+        "https://imgs.search.brave.com/XvQREnDH62oEwCzP1qCmkfW41drI4tuKDswmcQ17cvg/rs:fit:450:350:1/g:ce/aHR0cHM6Ly92aWdu/ZXR0ZTIud2lraWEu/bm9jb29raWUubmV0/L2Fzc2Fzc2luc2Ny/ZWVkL2ltYWdlcy8z/LzM5L05vdC1mb3Vu/ZC5qcGcvcmV2aXNp/b24vbGF0ZXN0P2Ni/PTIwMTEwNTE3MTcx/NTUy"
+      }
+      style={{ width: "30%", margin: "80px 0 0 35%" }}
+    />
+  );
+};
+
+export default NotFound;
